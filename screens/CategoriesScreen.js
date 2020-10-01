c3c6f44e6d66e63583395bdf9277c95e9c978649
garrett krage <garrett.krage@gmail.com>
2020-10-01 12:20:58 -0600

    added screen components boiler plate

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CategoriesScreen = props => {};
+
+const styles = StyleSheet.create({});
+
+export default CategoriesScreen;
---
+++ screens/FiltersScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FilterScreen = props => {};
+
+const styles = StyleSheet.create({});
+
+export default FilterScreen;
